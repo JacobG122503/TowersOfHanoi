7b919a6642547e01e8e079b6913cacaa4021f7cb
jacobg <jaybob1225@gmail.com>
2023-10-23 09:50:52 -0500

    initial commit

---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TowersOfHanoi
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello world :)");
+            Console.ReadLine();
+        }
+    }
+}
